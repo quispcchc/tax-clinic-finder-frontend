f38ff2de7b683ab7fc0ad47ff40a8d77809830a8
quispcchc <quisp@carlingtonchc.org>
2025-01-20 19:39:21 -0500

    implemented Reset Password Screen and added french language option

--- src/app/components/reset-password/reset-password.component.ts
+++ src/app/components/reset-password/reset-password.component.ts
@@ -6,31 +6,36 @@ import { AuthService } from '../../services/auth.service';
 @Component({
   selector: 'app-reset-password',
   standalone: false,
-  
   templateUrl: './reset-password.component.html',
-  styleUrl: './reset-password.component.scss'
+  styleUrls: ['./reset-password.component.scss']
 })
-export class ResetPasswordComponent implements OnInit{
+export class ResetPasswordComponent implements OnInit {
   resetPasswordForm!: FormGroup;
   token: string | null = null;
   message: string | null = null;
   isSuccess: boolean = false;
+  isPasswordUpdated: boolean = false;
 
   constructor(
     private fb: FormBuilder,
     private route: ActivatedRoute,
     private authService: AuthService,
     private router: Router
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this.resetPasswordForm = this.fb.group({
-      password: ['', [Validators.required, Validators.minLength(6),
-        this.authService.passwordValidator.bind(this)
-      ]]
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(6),
+          this.authService.passwordValidator.bind(this)
+        ]
+      ]
     });
 
-    this.route.paramMap.subscribe(params => {
+    this.route.paramMap.subscribe((params) => {
       this.token = params.get('token');
     });
   }
@@ -41,10 +46,19 @@ export class ResetPasswordComponent implements OnInit{
 
   onSubmit() {
     if (this.resetPasswordForm.valid && this.token) {
-      this.authService.resetPasswordWithToken(this.token, this.resetPasswordForm.value.password).subscribe(response => {
-        this.message = response.message;
-        this.isSuccess = (this.message == 'Password has been reset');
-      });
+      this.authService
+        .resetPasswordWithToken(this.token, this.resetPasswordForm.value.password)
+        .subscribe(
+          (response) => {
+            this.message = response.message;
+            this.isSuccess = this.message === 'Password has been reset';
+            this.isPasswordUpdated = this.isSuccess;
+          },
+          (error) => {
+            this.message = 'An error occurred while resetting the password.';
+            this.isSuccess = false;
+          }
+        );
     }
   }
 
