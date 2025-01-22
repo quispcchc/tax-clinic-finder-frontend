3ddbcd6759cd7cac2d5c86c08e9b4c978bef144d
quispcchc <quisp@carlingtonchc.org>
2025-01-21 20:05:41 -0500

    Implemented Filter component

--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -13,21 +13,119 @@ export class FilterComponent {
   @Input() isSidebarOpen: boolean = false;
 
   filters: {
-    appointmentAvailability?: string;
-    supportedTaxYears?: string;
-    pincode?: string;
-    language?: string;
-    years?: number;
-    province?: string;
-    type?: string;
+    appointmentType?: string;
+    supportedTaxYears: {
+      lastYear?: boolean;
+      lastTwoYears?: boolean;
+      lastThreeYears?: boolean;
+      all?: boolean;
+    };
+    provinces: { [key: string]: boolean };
+    specialTaxCases: {
+      selfEmployed?: boolean;
+      deceased?: boolean;
+      bankruptcy?: boolean;
+      imprisoned?: boolean;
+    };
+    incomeLevelRestrictions: {
+      hasRestriction?: boolean;
+      incomeLevel?: string;
+    };
+    languageOptions: {
+      french?: boolean;
+      english?: boolean;
+      arabic?: boolean;
+      other?: boolean;
+      otherLanguage?: string;
+    };
+    serviceDeliveryModes: {
+      inPerson?: boolean;
+      virtual?: boolean;
+      byAppointment?: boolean;
+      walkIn?: boolean;
+    };
+    clientCategories: {
+      newcomers?: boolean;
+      students?: boolean;
+      indigenousClients?: boolean;
+    };
+    catchmentArea: {
+      hasCatchmentArea?: boolean;
+      postalCode?: string;
+    };
+    appointmentBooking: {
+      onlineAppointment?: boolean;
+      call?: boolean;
+      inPerson?: boolean;
+    };
+    serviceDays: {
+      weekdays?: boolean;
+      weekends?: boolean;
+    };
+    serviceHours: {
+      workHours?: boolean;
+      afterHours?: boolean;
+    };
   } = {
-    appointmentAvailability: '',
-    supportedTaxYears: '',
-    pincode: '',
-    language: '',
-    years: 0,
-    province: '',
-    type: '',
+    appointmentType: '',
+    supportedTaxYears: {
+      lastYear: false,
+      lastTwoYears: false,
+      lastThreeYears: false,
+      all: false,
+    },
+    provinces: {
+      AB: false,
+      BC: false,
+      MB: false,
+      NB: false,
+      ON: false
+    },
+    specialTaxCases: {
+      selfEmployed: false,
+      deceased: false,
+      bankruptcy: false,
+      imprisoned: false,
+    },
+    incomeLevelRestrictions: {
+      hasRestriction: false,
+      incomeLevel: '',
+    },
+    languageOptions: {
+      french: false,
+      english: false,
+      arabic: false,
+      other: false,
+      otherLanguage: '',
+    },
+    serviceDeliveryModes: {
+      inPerson: false,
+      virtual: false,
+      byAppointment: false,
+      walkIn: false,
+    },
+    clientCategories: {
+      newcomers: false,
+      students: false,
+      indigenousClients: false,
+    },
+    catchmentArea: {
+      hasCatchmentArea: false,
+      postalCode: '',
+    },
+    appointmentBooking: {
+      onlineAppointment: false,
+      call: false,
+      inPerson: false,
+    },
+    serviceDays: {
+      weekdays: false,
+      weekends: false,
+    },
+    serviceHours: {
+      workHours: false,
+      afterHours: false,
+    }
   };
 
   constructor() {}
