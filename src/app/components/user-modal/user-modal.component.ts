262ee6e4e9567dc45e648a8ea6e61d3d8f1d8e69
quispcchc <quisp@carlingtonchc.org>
2025-02-03 20:01:45 -0500

    added role radio buttons

--- src/app/components/user-modal/user-modal.component.ts
+++ src/app/components/user-modal/user-modal.component.ts
@@ -31,6 +31,8 @@ export class UserModalComponent {
 
   emailPattern: string = '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$';
   userForm: FormGroup;
+  roles: string[] = ['Admin', 'User'];
+
 
   formFields: {
     label: string;
