e280cee3d226fd00dfffa9272ba0f96a4319a1a6
quispchc <quisp@carlingtonchc.org>
2025-02-28 16:10:45 -0500

    backend point

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://taxclinics-ottawa.nn.r.appspot.com',
+    apiUrl: 'https://taxclinics-ottawa.nn.r.appspot.com/api',
     googleMapsApiKey: 'AIzaSyBsU4uFrtBlhd8mFp-3hULtTJQietrYe4E',
 };
