4545898db2c18961a8875f46ba833bbcf6f528f2
quispchc <quisp@carlingtonchc.org>
2025-03-28 17:04:06 -0400

    updated environment files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://taxclinics-ottawa.nn.r.appspot.com/api',
+    apiUrl: 'https://taxinics.centre-ebo.com/api',
     googleMapsApiKey: 'AIzaSyBsU4uFrtBlhd8mFp-3hULtTJQietrYe4E',
 };
